46deff8b4d494c07e59ed0a843b9f09152cbb5e5
Andreas <andreas848@hotmail.com>
2018-04-23 11:36:05 +0200

    nvm fix of connection timeout didnt work
    Conflicts:
    	src/main/java/PresentationLayer/Command.java
    	src/main/webapp/WEB-INF/orderpage.jsp

--- src/test/Test.java
+++ src/test/Test.java
@@ -20,6 +20,10 @@ public class Test
         System.out.println("hej med dig");
         System.out.println("hej med dig");
         System.out.println("hej med dig");
+        System.out.println("hej med dig");
+        System.out.println("hej med chef");
+        
+        
     }
     
 }
